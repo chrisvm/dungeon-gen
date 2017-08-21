afb51f83f1bb29e36daf88ee883912b5cb8c1e4d
chrisvm <cvelez@wovenware.com>
2017-08-21 13:54:35 -0400

    renamed projects in solution

--- dungeon-gen/Program.cs
+++ dungeon-gen/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Drawing;
-using dungeon_gen.Bsp;
+using dungeon_gen_lib;
+using dungeon_gen_lib.Bsp;
+using dungeon_gen_lib.Rendering;
 
 namespace dungeon_gen
 {
@@ -27,49 +29,11 @@ namespace dungeon_gen
 		
 		private static void PrintToBitmap(BspNode nodeTree)
 		{
-			var bitmap = new Bitmap((int) nodeTree.BBox.Size.X, (int) nodeTree.BBox.Size.Y);
-			RenderToBitmap(nodeTree, bitmap);
+			var bitmap = new BitmapRenderer().Render(nodeTree);
 			var path = System.IO.Path.Combine(
 				Environment.GetFolderPath(Environment.SpecialFolder.Desktop),
 				"Example.png");
 			bitmap.Save(path);
-		}
-
-		private static void RenderToBitmap(BspNode nodeTree, Image bitmap, Graphics gfx = null)
-		{
-			if (gfx == null) {
-				gfx = Graphics.FromImage(bitmap);
-				gfx.FillRectangle(Brushes.Black, 0, 0, bitmap.Width, bitmap.Height);
-			}
-			
-			if (nodeTree.Room != null) {
-				var roomPos = nodeTree.Room.Position;
-				var roomSize = nodeTree.Room.Size;
-				gfx.FillRectangle(Brushes.AliceBlue, 
-					(float) roomPos.X, 
-					(float) roomPos.Y, 
-					(float) roomSize.X, 
-					(float) roomSize.Y);
-			}
-			
-			var points = GetPointsFromBBox(nodeTree.BBox);
-			gfx.DrawLine(Pens.Red, points[0], points[1]);
-			gfx.DrawLine(Pens.Red, points[1], points[2]);
-			gfx.DrawLine(Pens.Red, points[2], points[3]);
-			gfx.DrawLine(Pens.Red, points[3], points[0]);
-			
-			foreach (var child in nodeTree.Children) {
-				RenderToBitmap(child, bitmap, gfx);
-			}
-		}
-
-		private static Point[] GetPointsFromBBox(BoundaryBox bbox)
-		{
-			var pointA = bbox.Position.AsPoint;
-			var pointB = new Point((int) (bbox.Position.X + bbox.Size.X), (int) bbox.Position.Y);
-			var pointC = new Point((int) (bbox.Position.X + bbox.Size.X), (int) (bbox.Position.Y + bbox.Size.Y));
-			var pointD = new Point((int) bbox.Position.X, (int) (bbox.Position.Y + bbox.Size.Y));
-			return new [] { pointA, pointB, pointC, pointD };
 		}
 	}
 }

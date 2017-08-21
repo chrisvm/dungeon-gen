4f776b854dc99f9f50639a971dc4c23b954d4955
chrisvm <cvelez@wovenware.com>
2017-08-21 13:52:55 -0400

    moved files to dungeon-gen-lib

---
+++ dungeon-gen-gui/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.Drawing;
+using System.IO;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using dungeon_gen_lib.Bsp;
+using dungeon_gen_lib.Rendering;
+
+namespace WpfApplication1
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow
+	{
+		
+		private BinarySpacePartition _bsp;
+		private BinarySpacePartition Bsp => _bsp ?? (_bsp = new BinarySpacePartition {
+			MinimumSideSize = 100,
+			PrintDebug = false
+		});
+		
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void GenerateButton_OnClick(object sender, RoutedEventArgs e)
+		{
+			var mapWidth = 800;
+			var mapHeight = 800;
+			var tree = Bsp.Partition(new BoundaryBox(new Vector2(0, 0), new Vector2(mapWidth, mapHeight)));
+			var bitmap = BitmapRenderer.Instance.Render(tree);
+			MapImage.Width = bitmap.Width;
+			MapImage.Height = bitmap.Height;
+			MapImage.Source = BitmapToImageSource(bitmap);
+		}
+		
+		private BitmapImage BitmapToImageSource(Image bitmap)
+		{
+			using (var memory = new MemoryStream())
+			{
+				bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+				memory.Position = 0;
+				var bitmapimage = new BitmapImage();
+				bitmapimage.BeginInit();
+				bitmapimage.StreamSource = memory;
+				bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
+				bitmapimage.EndInit();
+				return bitmapimage;
+			}
+		}
+	}
+}

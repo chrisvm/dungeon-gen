3a44511d7918b69e3e963691bf54aa1a20576909
chrisvm <cvelez@wovenware.com>
2017-08-21 17:30:04 -0400

    created room connection class skeleton

--- dungeon-gen-lib/Room/RoomConnector.cs
+++ dungeon-gen-lib/Room/RoomConnector.cs
@@ -1,7 +1,20 @@
-﻿namespace dungeon_gen_lib.Room
+﻿using System;
+using dungeon_gen_lib.Bsp;
+
+namespace dungeon_gen_lib.Room
 {
+	public class RoomConnection
+	{
+		public Vector2 Start { get; set; }	
+		public Vector2 End { get; set; }	
+		public int Width { get; set; }
+	}
+	
 	public class RoomConnector
 	{
-		
+		public RoomConnection ConnectRooms(BspNode a, BspNode b)
+		{
+			throw new NotImplementedException();
+		}
 	}
 }

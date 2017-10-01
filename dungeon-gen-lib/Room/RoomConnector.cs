fc8fde6ece00e29224d45c17247af8a455ac7186
chrisvm <cvelez@wovenware.com>
2017-10-01 00:31:22 -0400

    calculating the corridor origin correctly

--- dungeon-gen-lib/Room/RoomConnector.cs
+++ dungeon-gen-lib/Room/RoomConnector.cs
@@ -12,19 +12,46 @@ namespace dungeon_gen_lib.Room
 	
 	public class RoomConnector
 	{
+		
+		public const int RoomRadius = 20;
+		private Random _random;
+		
+		public RoomConnector(Random random)
+		{
+			_random = random;
+		}
+		
 		public RoomConnection ConnectRooms(BspNode node)
 		{
 			var a = node.Children[0];
 			var b = node.Children[1];
+			
+			// calculate the range of values for posible starting endpoints 
+			// depending on the splitting axis of the boundary box
+			double axisMax, axisMin;
 			if (node.SplitDirection == SplitDirection.Vertical) {
-				var axisMin = Math.Min(a.Room.Position.Y, b.Room.Position.Y);
-				var axisMax = Math.Min(a.Room.Position.Y + a.Room.Size.Y, 
+				axisMax = Math.Min(a.Room.Position.Y, b.Room.Position.Y);
+				axisMin = Math.Min(a.Room.Position.Y + a.Room.Size.Y, 
 				                       b.Room.Position.Y + b.Room.Size.Y);
 			} else {
-				var axisMin = Math.Min(a.Room.Position.X, b.Room.Position.X);
-				var axisMax = Math.Min(a.Room.Position.X + a.Room.Size.X, 
+				axisMin = Math.Min(a.Room.Position.X, b.Room.Position.X);
+				axisMax = Math.Min(a.Room.Position.X + a.Room.Size.X, 
 				                       b.Room.Position.X + b.Room.Size.X);
 			}
+			
+			// create the real working range, taking into 
+			// account the constant radius of the room
+			var workingRangeMin = axisMin + RoomRadius;
+			var workingRangeMax = axisMax + RoomRadius;
+			var workingRangeDelta = workingRangeMax - workingRangeMin;
+			
+			var corridorOrigin = workingRangeMin + _random.NextDouble() * workingRangeDelta; 
+			
+			// todo: correctly calculate the start and end of the connetion based on the calc origin
+			
+			return new RoomConnection {
+				Width = RoomRadius
+			};
 		}
 	}
 }

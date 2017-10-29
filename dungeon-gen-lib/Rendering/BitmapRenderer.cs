b6dfce8cbd55c670dabedfe401735edd8882e5a2
chrisvm <cvelez@wovenware.com>
2017-10-29 19:25:25 -0400

    added highlighting to leaf parent nodes when rendering to bitmap

--- dungeon-gen-lib/Rendering/BitmapRenderer.cs
+++ dungeon-gen-lib/Rendering/BitmapRenderer.cs
@@ -1,5 +1,8 @@
-﻿using System.Drawing;
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.IO;
+using System.Linq;
 using System.Windows.Media.Imaging;
 using dungeon_gen_lib.Bsp;
 
@@ -10,17 +13,32 @@ namespace dungeon_gen_lib.Rendering
 		public Bitmap Render(BspNode tree)
 		{
 			var bitmap = new Bitmap((int) tree.bbox.size.x, (int) tree.bbox.size.y);
-			RecursiveRenderRoomsToBitmap(tree, bitmap);
+			var gfx = Graphics.FromImage(bitmap);
+			RecursiveRenderRooms(tree, gfx);
+			var leafParents = tree.AllNodesBeneath().Where((node, i) => {
+				var childrenAreLeafs = node.Children.Count != 0;
+				foreach (var child in node.Children) {
+					if (child.Children.Count != 0) childrenAreLeafs = false;
+				}
+				return childrenAreLeafs;
+			}).ToList();
+			RenderHighlightedRooms(leafParents, gfx);
 			return bitmap;
 		}
-		
-		private static void RecursiveRenderRoomsToBitmap(BspNode nodeTree, Image bitmap, Graphics gfx = null)
+
+		private static void RenderHighlightedRooms(List<BspNode> nodes, Graphics gfx)
 		{
-			if (gfx == null) {
-				gfx = Graphics.FromImage(bitmap);
-				gfx.FillRectangle(Brushes.Black, 0, 0, bitmap.Width, bitmap.Height);
+			const int penWidth = 4;
+			var pen = new Pen(Color.Azure, penWidth) {
+				DashPattern = new [] {1.0f , 1.0f}
+			};
+			foreach (var node in nodes) {
+				gfx.DrawPath(pen, GetGraphicsPath(node.bbox));	
 			}
-			
+		}
+
+		private static void RecursiveRenderRooms(BspNode nodeTree, Graphics gfx)
+		{
 			if (nodeTree.room != null) {
 				var roomPos = nodeTree.room.position;
 				var roomSize = nodeTree.room.size;
@@ -31,24 +49,32 @@ namespace dungeon_gen_lib.Rendering
 				                  (float) roomSize.y);
 			}
 			
-			var points = GetPointsFromBBox(nodeTree.bbox);
-			gfx.DrawLine(Pens.Red, points[0], points[1]);
-			gfx.DrawLine(Pens.Red, points[1], points[2]);
-			gfx.DrawLine(Pens.Red, points[2], points[3]);
-			gfx.DrawLine(Pens.Red, points[3], points[0]);
+			var path = GetGraphicsPath(nodeTree.bbox);
+			gfx.DrawPath(Pens.Red, path);
 			
 			foreach (var child in nodeTree.Children) {
-				RecursiveRenderRoomsToBitmap(child, bitmap, gfx);
+				RecursiveRenderRooms(child, gfx);
 			}
 		}
 
-		private static Point[] GetPointsFromBBox(BoundaryBox bbox)
+		private static Point[] GetPoints(BoundaryBox bbox)
 		{
 			var pointA = bbox.position.AsPoint;
 			var pointB = new Point((int) (bbox.position.x + bbox.size.x), (int) bbox.position.y);
 			var pointC = new Point((int) (bbox.position.x + bbox.size.x), (int) (bbox.position.y + bbox.size.y));
 			var pointD = new Point((int) bbox.position.x, (int) (bbox.position.y + bbox.size.y));
 			return new [] { pointA, pointB, pointC, pointD };
+		}
+		
+		private static GraphicsPath GetGraphicsPath(BoundaryBox bbox)
+		{
+			var points = GetPoints(bbox);
+			var gfxPath = new GraphicsPath();
+			gfxPath.AddLine(points[0], points[1]);
+			gfxPath.AddLine(points[1], points[2]);
+			gfxPath.AddLine(points[2], points[3]);
+			gfxPath.AddLine(points[3], points[0]);
+			return gfxPath;
 		}
 		
 		/// <summary>

ac5b5cf4667f565da94bcf84385c820f241d5266
chrisvm <cvelez@wovenware.com>
2017-10-29 19:50:56 -0400

    moved RoomConnection class to its own file

---
+++ dungeon-gen-lib/Room/RoomConnection.cs
@@ -0,0 +1,11 @@
+﻿using dungeon_gen_lib.Bsp;
+
+namespace dungeon_gen_lib.Room
+{
+	public class RoomConnection
+	{
+		public Vector2 Start { get; set; }	
+		public Vector2 End { get; set; }	
+		public int Width { get; set; }
+	}
+}
--- dungeon-gen-lib/Room/RoomConnector.cs
+++ dungeon-gen-lib/Room/RoomConnector.cs
@@ -3,13 +3,6 @@ using dungeon_gen_lib.Bsp;
 
 namespace dungeon_gen_lib.Room
 {
-	public class RoomConnection
-	{
-		public Vector2 Start { get; set; }	
-		public Vector2 End { get; set; }	
-		public int Width { get; set; }
-	}
-	
 	public class RoomConnector
 	{
 		public int RoomRadius { get; set; } 

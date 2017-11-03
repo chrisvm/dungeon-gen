6bd5b70e9a35f039c1c7bffa76953a6b913acd96
chrisvm <cvelez@wovenware.com>
2017-11-03 13:19:32 -0400

    GetGraphicsPath method implemented for RoomConnection

--- dungeon-gen-lib/Room/RoomConnection.cs
+++ dungeon-gen-lib/Room/RoomConnection.cs
@@ -13,27 +13,31 @@ namespace dungeon_gen_lib.Room
 		public SplitDirection SplitDirection { get; set; }
 		public int Width { get; set; }
 		
-		private Point[] GetPoints()
+		private static Point[] GetPoints(RoomConnection roomConnection)
 		{
-			var delta = End - Start;
+			var delta = roomConnection.End - roomConnection.Start;
 			var perpendicular = delta.Clone();
 			perpendicular.Normalize();
 			perpendicular.Rotate(Math.PI / 2);
-			perpendicular.Scale(Width);
+			perpendicular.Scale(roomConnection.Width);
 			var points = new [] {
-				(Start - perpendicular).AsPoint,
-				(End - perpendicular).AsPoint,
-				(End + perpendicular).AsPoint,
-				(Start + perpendicular).AsPoint
+				(roomConnection.Start - perpendicular).AsPoint,
+				(roomConnection.End - perpendicular).AsPoint,
+				(roomConnection.End + perpendicular).AsPoint,
+				(roomConnection.Start + perpendicular).AsPoint
 			};
 			return points;
 		}
 		
 		public GraphicsPath GetGraphicsPath()
 		{
-			var path = new GraphicsPath();
-			var delta = End - Start;
-			
+			var points = GetPoints(this);
+			var gfxPath = new GraphicsPath();
+			gfxPath.AddLine(points[0], points[1]);
+			gfxPath.AddLine(points[1], points[2]);
+			gfxPath.AddLine(points[2], points[3]);
+			gfxPath.AddLine(points[3], points[0]);
+			return gfxPath;
 		}
 	}
 }

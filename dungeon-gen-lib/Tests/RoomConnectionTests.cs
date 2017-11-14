e7816d383c232586bd9c21cc4388bd374d5a2738
chrisvm <cvelez@wovenware.com>
2017-11-14 02:52:55 -0400

    added unit test for connection endpoints

---
+++ dungeon-gen-lib/Tests/RoomConnectionTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using dungeon_gen_lib.Bsp;
+using dungeon_gen_lib.Room;
+using NUnit.Framework;
+
+namespace dungeon_gen_lib.Tests
+{
+	public class RoomConnectionTests
+	{
+		private uint Iterations { get; } = 10000;
+		
+		[Test]
+		public void ShouldCorrectlyConnectRoom()
+		{
+			var random = new Random();
+			var roomConnector = new RoomConnector(random);
+			var bspNode = TestingTools.CreateVerticallySplitedNode();
+			
+			TestingTools.IterateAction(Iterations, () => {
+				var roomConnection = roomConnector.ConnectRooms(bspNode);
+				Console.WriteLine(roomConnection);
+			});
+		}
+		
+		[Test]
+		public void ShouldCorrectlyCreateEndpointsOnPerpendicularSplittingAxis()
+		{
+			var random = new Random();
+			var roomConnector = new RoomConnector(random);
+			var bspNode = TestingTools.CreateVerticallySplitedNode();
+			
+			var expectedStart = bspNode.Children[0].room.position.x + bspNode.Children[0].room.size.x;
+			var expectedEnd = bspNode.Children[1].room.position.x;
+			TestingTools.IterateAction(Iterations, () => {
+				var roomConnection = roomConnector.ConnectRooms(bspNode);
+				Assert.AreEqual(expectedStart, roomConnection.Start.x);
+				Assert.AreEqual(expectedEnd, roomConnection.End.x);
+			});
+		}
+	}
+}
---
+++ dungeon-gen-lib/Tests/TestingTools.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using dungeon_gen_lib.Bsp;
+using dungeon_gen_lib.Room;
+
+namespace dungeon_gen_lib.Tests
+{
+	/// <summary>
+	/// Collection of methods used in unit tests.
+	/// </summary>
+	public static class TestingTools
+	{
+		/// <summary>
+		/// Iterates a given action a number of iterations.
+		/// </summary>
+		/// <param name="iterations"></param>
+		/// <param name="action"></param>
+		public static void IterateAction(uint iterations, Action action)
+		{
+			for (var iteration = 0; iteration < iterations; iteration++) {
+				action();
+			}
+		}
+		
+		/// <summary>
+		/// Creates a vertically splited node with BoundaryBox of size 100, 100.
+		/// </summary>
+		/// <returns></returns>
+		public static BspNode CreateVerticallySplitedNode()
+		{
+			var boundaryBox = new BoundaryBox(new Vector2(0, 0), new Vector2(100, 100));
+			var bspNode = new BspNode(boundaryBox);
+			
+			var aBoundaryBox = new BoundaryBox(new Vector2(0, 0), new Vector2(50, 100));
+			var a = new BspNode(aBoundaryBox) {
+				room = new BoundaryBox {
+					position = new Vector2(12.5, 25),
+					size = new Vector2(25, 50)
+				}
+			};
+
+			var bBoundaryBox = new BoundaryBox(new Vector2(50, 0), new Vector2(50, 100));
+			var b = new BspNode(bBoundaryBox) {
+				room = new BoundaryBox {
+					position = new Vector2(62.5, 25),
+					size = new Vector2(25, 50)
+				}
+			};
+
+			bspNode.AddChild(a);
+			bspNode.AddChild(b);
+			bspNode.splitDirection = SplitDirection.Vertical;
+			
+			return bspNode;
+		}
+	}
+	
+	/// <summary>
+	/// Expose to expose private methods in <see cref="RoomCreator"/> class.
+	/// </summary>
+	public class RoomCreatorExpose : RoomCreator
+	{
+		public RoomCreatorExpose(Random random) : base(random) {}
+            
+		public BoundaryBox CreateRoom(BoundaryBox bbox)
+		{
+			return _CreateRoom(bbox, Random);
+		}
+	}
+}

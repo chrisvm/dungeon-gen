7c07fc5ff7ba93b3f31604479a994c065f1aa77c
chrisvm <cvelez@wovenware.com>
2017-11-27 21:16:51 -0400

    refactored code for unit test

--- dungeon-gen-lib/Room/RoomConnector.cs
+++ dungeon-gen-lib/Room/RoomConnector.cs
@@ -30,28 +30,17 @@ namespace dungeon_gen_lib.Room
 			var a = node.Children[0];
 			var b = node.Children[1];
 			
-			// calculate the intersection of the two given bboxes
-			var intersection = new double[2];
-			if (node.splitDirection == SplitDirection.Vertical) {
-				intersection[0] = Math.Max(a.bbox.position.y, b.bbox.position.y);
-				intersection[1] = Math.Min(a.bbox.position.y + a.bbox.size.y, b.bbox.position.y + b.bbox.size.y);
-			} else {
-				intersection[0] = Math.Max(a.bbox.position.x, b.bbox.position.x);
-				intersection[1] = Math.Min(a.bbox.position.x + a.bbox.size.x, b.bbox.position.x + b.bbox.size.x);
-			}
-			
-			// no intersection between the bboxes,
-			// a z-corridor is needed
-			if (intersection[0] > intersection[1]) {
+			// calculate the intersection of the two given roomes
+			var intersection = CalculateIntersection(node.splitDirection, a.room, b.room);
+
+			// no intersection between the roomes, a z-corridor is needed, also, if there's 
+			// some intersection space but not enough to create a axis corridor, create a z-corridor
+			var noIntersection = intersection[0] > intersection[1];
+			var noAvailableSpaceForIntersection = intersection[1] - intersection[0] < 2 * RoomRadius;
+			if (noIntersection || noAvailableSpaceForIntersection) {
 				throw new NotImplementedException("Z Corridors are not implemented.");
 			}
-			
-			// if there's some intersection space but not enough to 
-			// create a axis corridor, create a z-corridor
-			if (intersection[1] - intersection[0] < 2 * RoomRadius) {
-				throw new NotImplementedException("Z Corridors are not implemented.");
-			}
-			
+		
 			// create the real working range, taking into 
 			// account the constant radius of the room
 			var workingRange = new [] {
@@ -69,14 +58,28 @@ namespace dungeon_gen_lib.Room
 			
 			// calculate the start and end of the connetion based on the calc origin
 			if (node.splitDirection == SplitDirection.Vertical) {
-				connection.Start = new Vector2(a.bbox.position.x + a.bbox.size.x, corridorOrigin);
-				connection.End = new Vector2(b.bbox.position.x + b.bbox.size.x, corridorOrigin);
+				connection.Start = new Vector2(a.room.position.x + a.room.size.x, corridorOrigin);
+				connection.End = new Vector2(b.room.position.x + b.room.size.x, corridorOrigin);
 			} else {
-				connection.Start = new Vector2(corridorOrigin, a.bbox.position.y + a.bbox.size.y);
-				connection.End = new Vector2(corridorOrigin, b.bbox.position.y + b.bbox.size.y);
+				connection.Start = new Vector2(corridorOrigin, a.room.position.y + a.room.size.y);
+				connection.End = new Vector2(corridorOrigin, b.room.position.y + b.room.size.y);
 			}
 			
 			return connection;
+		}
+
+		private static double[] CalculateIntersection(SplitDirection splitDirection, BoundaryBox a, BoundaryBox b)
+		{
+			var intersection = new double[2];
+			if (splitDirection == SplitDirection.Vertical) {
+				intersection[0] = Math.Max(a.position.y, b.position.y);
+				intersection[1] = Math.Min(a.position.y + a.size.y, b.position.y + b.size.y);
+			}
+			else {
+				intersection[0] = Math.Max(a.position.x, b.position.x);
+				intersection[1] = Math.Min(a.position.x + a.size.x, b.position.x + b.size.x);
+			}
+			return intersection;
 		}
 	}
 }
